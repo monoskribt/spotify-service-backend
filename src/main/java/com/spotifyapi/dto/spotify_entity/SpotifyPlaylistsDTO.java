d1395f2961fac4d23e2908b6034c33584c800682
monoskribt <kozub.dima3@gmail.com>
2025-03-06 22:18:51 +0100

    new version of tests

--- src/main/java/com/spotifyapi/dto/spotify_entity/SpotifyPlaylistsDTO.java
+++ src/main/java/com/spotifyapi/dto/spotify_entity/SpotifyPlaylistsDTO.java
@@ -3,11 +3,13 @@ package com.spotifyapi.dto.spotify_entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SpotifyPlaylistsDTO {
 
     private String id;
--- src/main/java/com/spotifyapi/dto/spotify_entity/SpotifyReleaseDTO.java
+++ src/main/java/com/spotifyapi/dto/spotify_entity/SpotifyReleaseDTO.java
@@ -2,9 +2,13 @@ package com.spotifyapi.dto.spotify_entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class SpotifyReleaseDTO {
 
     private String id;
